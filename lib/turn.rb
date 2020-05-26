0f3598048fb5834a6c766c92b0e683b94203d474
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-26 20:18:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,9 +40,9 @@
     
       if valid_move? == true
         
-        display_board(board)
-        puts "Please enter 1-9:"
-      else
-        puts "Please enter 1-9:"
+        #display_board(board)
+        #puts "Please enter 1-9:"
+      #else
+        #puts "Please enter 1-9:"
     end
   end
